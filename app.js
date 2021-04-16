b6169ed25f4cfc35d615bb3d47943e7c9b93272a
cgray888 <cnnr.gray@gmail.com>
2021-04-16 13:08:11 -0700

    Basic front end will load

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// middleware
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// test
+this.router.get('/plants', function(req, res, next) {
+    res.send({message: "Well done"})
+});
 
 module.exports = app;
--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -5,13 +5,15 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
 const PlantList = () => {
-  const [plants, setPlants] = useEffect([]);
+  const [plants, setPlants] = useState([]);
 
   useEffect(() => {
     (async () => {
       try {
         const response = await ky("/plants").json();
         setPlants(response);
+        // test
+        console.log(response);
       } catch (error) {
         console.error("API GET Error: " + error);
       }
